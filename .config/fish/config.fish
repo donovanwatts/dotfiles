e67e7aedb0ae879e3d01272a458591b02e977ab9
Donovan Watts <me@donovanwatts.net>
2021-12-12 07:11:00 -0700

    Fish config updates
    Suppress welcome message and increase directory length

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
+#######################
+#  Fish Shell Config  #
+#######################
+
+set fish_greeting                   # Disable greeting message
+set -g fish_prompt_pwd_dir_length 9 # Set prompt directory name length
+
 if status --is-login
 	set -gx PATH $PATH ~/linux/bin
 end
