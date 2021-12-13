3683b754f69dffacdf41024578b3c23ad4bdef71
Donovan Watts <me@donovanwatts.net>
2021-12-13 07:00:21 -0700

    changed path to dw

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ set fish_greeting                   # Disable greeting message
 set -g fish_prompt_pwd_dir_length 9 # Set prompt directory name length
 
 if status --is-login
-	set -gx PATH $PATH ~/linux/bin
+	set -gx PATH $PATH ~/dw/bin
 end
 
 neofetch
